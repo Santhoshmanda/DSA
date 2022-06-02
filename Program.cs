fbc49e93a47c1e2e349b5d9227cde06d3f2aa639
Santhosh Manda <Santhosh7598@outlook.com>
2022-06-02 23:45:22 +0530

    initial setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Stack_DSA;
+
+namespace DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Stack_DSA.NGE.Method();
+            
+        }
+    }
+}
---
+++ Stack/NextGreaterElement.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+
+namespace Stack_DSA
+{
+    public class NGE{
+
+        public static void Method()
+        {
+                Stack<int> st= new Stack<int>();
+                st.Push(2);
+                Console.WriteLine(st.Peek());
+            
+        }
+       
+    }
+}
